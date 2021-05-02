7acd01f355a74b98700b1cd9cdf50c41a7d46a19
Lucas Teixeira <lucas.vieira@brmalls.com.br>
2021-05-02 01:59:10 -0300

    - feat: implementando criação de filas automáticas. - feat: simulando erro para deadLetter.

--- consumer/src/main/java/com/lucasvieira/consumer/service/ConsumerService.java
+++ consumer/src/main/java/com/lucasvieira/consumer/service/ConsumerService.java
@@ -3,5 +3,5 @@ package com.lucasvieira.consumer.service;
 import com.lucasvieira.consumer.dto.MessageQueue;
 
 public interface ConsumerService {
-    void action(MessageQueue message) throws Exception;
+    void action(MessageQueue message);
 }
--- consumer/src/main/java/com/lucasvieira/consumer/service/impl/ConsumerServiceImpl.java
+++ consumer/src/main/java/com/lucasvieira/consumer/service/impl/ConsumerServiceImpl.java
@@ -2,13 +2,18 @@ package com.lucasvieira.consumer.service.impl;
 
 import com.lucasvieira.consumer.dto.MessageQueue;
 import com.lucasvieira.consumer.service.ConsumerService;
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ConsumerServiceImpl implements ConsumerService {
 
     @Override
-    public void action(MessageQueue message) throws Exception {
+    public void action(MessageQueue message) {
+        if ("teste".equalsIgnoreCase(message.getText())) {
+            //Simulando exception
+            throw new AmqpRejectAndDontRequeueException("Erro");
+        }
         System.out.println(message.getText());
     }
 }
